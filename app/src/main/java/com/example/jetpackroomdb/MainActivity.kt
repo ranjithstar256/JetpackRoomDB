7e65fc023470143f85bcd45b9fddea770cbb79c4
Ranjith <ranjithstar256@gmail.com>
2023-02-21 14:09:04 +0530

    added coroutine and removed MVVM Made code simple just called DAO functions from MainActivity.kt

--- app/src/main/java/com/example/jetpackroomdb/MainActivity.kt
+++ app/src/main/java/com/example/jetpackroomdb/MainActivity.kt
@@ -1,40 +1,97 @@
 package com.example.jetpackroomdb
 
-import android.content.Context
-import android.os.AsyncTask
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewModelScope
+import androidx.room.Room
 import com.example.jetpackroomdb.ui.theme.JetpackRoomDBTheme
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("CoroutineCreationDuringComposition")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
             JetpackRoomDBTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting(applicationContext)
+                    lateinit var todoDao: TodoDatabaseDao
 
+                    var db: TodoDatabase =
+                        Room.inMemoryDatabaseBuilder(applicationContext, TodoDatabase::class.java)
+                            .allowMainThreadQueries()
+                            .build()
+                    todoDao = db.todoDao()
+
+                    var r = false
+                    Column {
+
+                        var s1 by remember {
+                            mutableStateOf("abc")
+                        }
+                        var s2 by remember {
+                            mutableStateOf("xyz")
+                        }
+
+                        TextField(value = s1, onValueChange = { s1 = it })
+                        TextField(value = s2, onValueChange = { s2 = it })
+
+                        Button(onClick = {
+                            r = true
+                        s2=s1}) {
+                            Text(text = "Save")
+
+                        }
+                        if (r) {
+                            val todoItem = TodoItem(itemName = s2, isDone = false)
+                            val scope = rememberCoroutineScope()
+                            scope.launch {
+                                todoDao.insert(todoItem)
+                            }
+                        } else{
+                            val todoItem = TodoItem(itemName = s2, isDone = false)
+                            val scope = rememberCoroutineScope()
+                            scope.launch {
+                                todoDao.insert(todoItem)
+                            }
+                        }
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
- fun Greeting(name: Context) {
-    
+    @SuppressLint("CoroutineCreationDuringComposition")
+    @Composable
+    fun Greeting(todoDatabaseDao: TodoDatabaseDao) {
+
+        val scope = rememberCoroutineScope()
+        scope.launch {
+
+            try {
+
+                val todoItem = TodoItem(itemName = "some Item", isDone = false)
+                todoDatabaseDao.insert(todoItem)
+
+                // var g = todoDatabaseDao.getById(1)
+                //  todoDatabaseDao.deleteAllTodos()
+
+                //    var c = g?.itemName
+                //    Log.i("dddffdsfd", "item is : " + c)
+
+            } catch (ex: Exception) {
+                println("cancelled")
+            }
+        }
+    }
 }
